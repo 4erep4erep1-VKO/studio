e44546c3fc7918b60ad2a27d25355a09e5c4c497
Nicolas Bokovs <nicolasbokovs@gmail.com>
2026-04-08 08:17:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7668243720-e1595",
+  "appId": "1:261445413448:web:fb38775788479f1397867d",
+  "apiKey": "AIzaSyDP4UrDwJyrz7e4H-F87fAJ61ULM-EHqBk",
+  "authDomain": "studio-7668243720-e1595.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "261445413448"
+};
