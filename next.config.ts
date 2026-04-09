a3e7b3a394ba9e3b8eb2720cf6d30480041d566a
4erep4erep1-VKO <4erep4erep1@gmail.com>
2026-04-09 19:20:38 +0000

    Add Supabase domain to Next.js image remotePatterns
    - Added ehrfzwhawnqyocbthjmb.supabase.co to images.remotePatterns
    - This allows images uploaded to Supabase Storage to display in the app
    - Fixes image loading issues from Supabase bucket

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'ehrfzwhawnqyocbthjmb.supabase.co',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
