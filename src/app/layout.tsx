41becd8e4479f8c126ae3188896a063021835b99
4erep4erep1-VKO <4erep4erep1@gmail.com>
2026-07-02 18:40:47 +0000

    увеличение размера логотипа

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,9 +26,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               <Image
                 src="/logo.png"
                 alt="Монтажка PRO"
-                width={40}
-                height={40}
-                className="h-10 w-10 object-contain"
+                width={56}
+                height={56}
+                className="h-14 w-14 object-contain"
               />
             </Link>
             <ThemeToggle />
